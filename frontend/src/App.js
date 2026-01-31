a1d1e9b5d44bd6e6e17b02a9597383d41491d270
AdrianMorack <acmorack@gmail.com>
2026-01-31 11:34:44 -0600

    Fixes for web launch

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,5 @@
 import './App.css';
-import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route, Link } from 'react-router-dom';
 import Home from './pages/Home';
 import Skills from './pages/Skills';
 import Contact from './pages/Contact';
